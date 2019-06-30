302ee96aa32d6e2c34272fb415488b5880391f2c
DaRican <jcastillo8150@gmail.com>
2019-06-30 00:02:46 -0500

    js array of trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,120 @@
+var question1 = {
+    question: 'What was specifically designed to be the size and weight of a baseball?',
+    answer: "Hand Grenade",
+    wrongAnswerOne: "Coffee Mug",
+    wrongAnswerTwo: "Walkie Talkie",
+    wrongAnswerThree: "PS4 Controller"
+};
+
+var question2 = {
+    question: 'Who was the first Major League player to pitch a ball over 100 mph?',
+    answer: "Nolan Ryan",
+    wrongAnswerOne: "Dwight Gooden",
+    wrongAnswerTwo: "Tom Seaver",
+    wrongAnswerThree: "Roger Clemens"
+};
+
+var question3 = {
+    question: 'What player was nicknamed "Mr. November"?',
+    answer: "Derek Jeter",
+    wrongAnswerOne: "Reggie Jackson",
+    wrongAnswerTwo: "",
+    wrongAnswerThree: ""
+};
+
+var question4 = {
+    question: 'Which pitcher had no right hand?',
+    answer: "Jim Abbot",
+    wrongAnswerOne: "Nolan Ryan",
+    wrongAnswerTwo: "Lefty Gomez",
+    wrongAnswerThree: "Curt Schilling"
+};
+
+var question5 = {
+    question: 'How many baseballs could John Bench hold in one hand?',
+    answer: "Seven",
+    wrongAnswerOne: "Five",
+    wrongAnswerTwo: "Six",
+    wrongAnswerThree: "Eight"
+};
+
+var question6 = {
+    question: 'What is the most stolen base?',
+    answer: "Second",
+    wrongAnswerOne: "First",
+    wrongAnswerTwo: "Third",
+    wrongAnswerThree: "Home"
+};
+
+var question7 = {
+    question: 'Who was the first Puerto Rican to receive the World Series MVP award?',
+    answer: "Roberto Clemente",
+    wrongAnswerOne: "Carlos Beltran",
+    wrongAnswerTwo: "Jorge Posada",
+    wrongAnswerThree: "Joe Colon"
+};
+
+var question8 = {
+    question: 'What did Pittsburgh Pirate pitcher Dock Ellis claim to have done on June 12, 1970?',
+    answer: "Pitch a No hitter on LSD",
+    wrongAnswerOne: "Talk to the Ghost of Babe Ruth",
+    wrongAnswerTwo: "Bribed an Umpire",
+    wrongAnswerThree: "Pitch in his sleep"
+};
+
+var question9 = {
+    question: 'Who holds the all-time home run and RBI records among Puerto Rican players',
+    answer: "Carlos Delgado",
+    wrongAnswerOne: "Ivan Rodriguez",
+    wrongAnswerTwo: "Juan Gonzalez",
+    wrongAnswerThree: "Bernie Williams"
+};
+
+var question10 = {
+    question: 'Which of the following pitchers is not left-handed?',
+    answer: "Roger Clemens",
+    wrongAnswerOne: "Randy Johnson",
+    wrongAnswerTwo: "White Ford",
+    wrongAnswerThree: "Sandy Koufax"
+};
+
+var question11 = {
+    question: 'What is the most expensive baseball card?',
+    answer: "Honus Wagner",
+    wrongAnswerOne: "Babe Ruth",
+    wrongAnswerTwo: "Micky Mantle",
+    wrongAnswerThree: "Derek Jeter"
+};
+
+var question12 = {
+    question: 'Why was Babe Ruth\'s Jersey Number 3? ',
+    answer: "He batted third",
+    wrongAnswerOne: "He was born on the Third of March 1903",
+    wrongAnswerTwo: "For the Holy Trinity",
+    wrongAnswerThree: "He had three brothers"
+};
+
+var question13 = {
+    question: 'What is the most popular food item',
+    answer: "Hot Dogs",
+    wrongAnswerOne: "Pizza",
+    wrongAnswerTwo: "Hamburgers",
+    wrongAnswerThree: "Nachos"
+};
+
+var question14 = {
+    question: 'What was Babe Ruth\'s first name?',
+    answer: "George",
+    wrongAnswerOne: "Henry",
+    wrongAnswerTwo: "William",
+    wrongAnswerThree: "Bobby"
+};
+
+var question15 = {
+    question: 'Who was the first girl to pitch a shutout in the Little League Baseball World Series?',
+    answer: "Mo'ne Davis",
+    wrongAnswerOne: "Emma March",
+    wrongAnswerTwo: "Victoria Roche",
+    wrongAnswerThree: "Eliska Stejskalova"
+};
+
